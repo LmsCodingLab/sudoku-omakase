2e9a131af3ffcf123a53e777c9e0ddb8ff449aac
Philipp Knoch <knokh.ph@gmail.com>
2026-02-28 17:44:23 +0100

    combined datasets, added commets and fixed a bug in the transformation

--- src/warmup.py
+++ src/warmup.py
@@ -1,33 +1,45 @@
-import torch
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
+
 
 # Preparing the data for training or in other words warming up ;)
-def warmup() -> tuple[DataLoader, DataLoader]:
-  # transform data from different datasets
+def warmup() -> DataLoader:
+  """
+  Prepare the EMNIST and SVHN datasets for training by applying necessary transformations and creating DataLoader.
+
+  Parameters:
+  - None
+
+  Returns:
+  - DataLoader: A DataLoader for the combined EMNIST and SVHN datasets.
+  """
+
+  # Define transofrmation of datasets to be compatible with the model and each other (grayscale, 32x32, normalized)
   emnist_transform = transforms.Compose([
-    transforms.Resize((32, 32)),
+    transforms.Resize(size=(32, 32)),
+    transforms.RandomInvert(p=1),
+    transforms.Lambda(lambda x: x.transpose(1, 2)),
     transforms.ToTensor(),
-    transforms.Lambda(lambda x: 1 - x),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize(mean=(0.5,), std=(0.5,))
   ])
 
   svhn_transform = transforms.Compose([
     transforms.Grayscale(),
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize(mean=(0.5,), std=(0.5,))
   ])
 
   # Load the datasets
   emnist_train = datasets.EMNIST(root="./datasets", split="digits", train=True, download=True, transform=emnist_transform)
   svhn_train = datasets.SVHN(root="./datasets", split="train", download=True, transform=svhn_transform)
 
-  emnist_dataLoader = DataLoader(emnist_train, batch_size=64, shuffle=True)
-  svhn_dataLoader = DataLoader(svhn_train, batch_size=64, shuffle=True)
+  # Combine the datasets and create a DataLoader
+  combined_dataset = ConcatDataset(datasets=[emnist_train, svhn_train])
+  data_loader = DataLoader(dataset=combined_dataset, batch_size=64, shuffle=True)
 
-  return emnist_dataLoader, svhn_dataLoader
+  return data_loader
 
 if __name__ == "__main__":
-  emnist_loader, svhn_loader = warmup()
-  print(f"EMNIST DataLoader: {len(emnist_loader)} batches")
-  print(f"SVHN DataLoader: {len(svhn_loader)} batches")
+  data_loader = warmup()
+  print(f"Number of batches in combined dataset: {len(data_loader)}")
+
