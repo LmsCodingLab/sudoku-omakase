413971368ea44f075468fcc49e51cdda55df6117
Philipp <111361294+kn0kh@users.noreply.github.com>
2026-03-11 20:39:55 +0100

    74 add complete vision (#80)
    Co-authored-by: Konstantin <73552521+konnybonny2307@users.noreply.github.com>

---
+++ src/sudoku_omakase/vision/cell_extraction.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+import numpy.typing as npt
+
+from sudoku_omakase.helpers.predictor import guess_num # TODO move to utils later
+from sudoku_omakase.vision.sudoku_image import ModelType, SudokuImage, Image
+
+def extract_fields(sudoku_image: SudokuImage) -> list[Image]:
+    """
+    Extracts the 81 individual fields from the warped sudoku grid image.
+
+    Parameters:
+    - sudoku_image: SudokuImage, the warped sudoku grid image.
+
+    Returns:
+    - list of np.ndarray, the extracted field images.
+    """
+    if sudoku_image.warped is None:
+        raise RuntimeError("Image must be preprocessed before extracting fields. Call preprocess() method first.")
+
+    fields = []
+    field_size = sudoku_image.size // 9
+    for row in range(9):
+        for col in range(9):
+            x_start, y_start = col * field_size, row * field_size
+            field = sudoku_image.warped[y_start:y_start + field_size, x_start:x_start + field_size]
+            fields.append(field)
+
+    return fields
+
+def resize_fields(fields: list[Image]) -> list[Image]:
+    """
+        Resizes the extracted field images to 32x32 pixels, which is the input size expected by the models.
+
+        Parameters:
+        - fields: list of np.ndarray, the extracted field images.
+
+        Returns:
+        - list of np.ndarray, the resized field images.
+    """
+    if not fields:
+        return []
+    
+    CROP_MARGIN = max(1, fields[0].shape[0] // 8) # Crop 1/8 of the field size or 1 from each side to remove grid lines 
+    resized_fields = []
+    for field in fields:
+        cropped_field = field[CROP_MARGIN:-CROP_MARGIN, CROP_MARGIN:-CROP_MARGIN] # Crop the margin from each side to remove grid lines
+        resized_field = cv2.resize(cropped_field, (32, 32), interpolation=cv2.INTER_AREA)
+        resized_fields.append(resized_field)
+
+    return resized_fields
+
+def extract_numbers(sudoku: list[Image], model_type: ModelType) -> npt.NDArray[np.int8]:
+    """
+        Uses the specified model to predict the number in each field of the sudoku grid.
+
+        Parameters:
+        - sudoku: list of np.ndarray, the list of field images.
+        - model_type: ModelType, the type of model to use for prediction.
+
+        Returns:
+        - np.ndarray, a 9x9 array representing the predicted numbers in the sudoku grid (0 for empty/uncertain fields).
+    """
+    if len(sudoku) != 81:
+        raise ValueError(f"Invalid number of fields: {len(sudoku)}. Expected 81 fields for a 9x9 sudoku grid.")
+    
+    numbers = []
+    batch = []
+    for field in sudoku:
+        num = guess_num(data=field, model_type=str(model_type)) # TODO change to model_type=model_type after fixing guess_num function to accept ModelType enum instead of string
+        batch.append(num)
+        if len(batch) == 9:
+            numbers.append(batch)
+            batch = []
+    result = np.array(numbers, dtype=np.int8)
+
+    return result
---
+++ src/sudoku_omakase/vision/preprocess.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+import numpy.typing as npt
+from typing import cast
+
+from sudoku_omakase.vision.sudoku_image import Image, SudokuImage
+
+
+def prepare_image(sudoku_image: SudokuImage) -> Image:
+	"""
+		Reads the input image, converts it to grayscale, applies a Gaussian blur, and then applies adaptive thresholding to prepare it for contour detection.
+
+		Parameters:
+		- sudoku_image: SudokuImage, the sudoku image to prepare.
+
+		Returns:
+		- np.ndarray, the processed image.
+	"""
+
+	# Load the image convert to grayscale and blur it
+	image = cv2.imread(str(sudoku_image.source), flags=0)
+	if image is None:
+		raise FileNotFoundError(f"Image not found at path: {sudoku_image.source}")
+
+	image = cv2.GaussianBlur(src=image, ksize=(7, 7), sigmaX=0)
+
+	# Apply adaptive thresholding to get a binary image
+	image = cv2.adaptiveThreshold(
+		src=image, 
+		maxValue=255, 
+		adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+		thresholdType=cv2.THRESH_BINARY_INV, 
+		blockSize=13, 
+		C=8
+	)
+
+	return cast(Image, image)
+
+def detect_grid(image: Image) -> npt.NDArray[np.float32]:
+	"""
+		Detects the largest contour in the image, which is assumed to be the sudoku grid, and returns its corners.
+
+		Parameters:
+		- image: np.ndarray, the processed image from which to detect the grid.
+
+		Returns:
+		- np.ndarray, the corners of the detected grid.
+	"""
+
+	# Connect broken grid lines / border so contours become closed
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+	closed_image = cast(Image, cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=2))
+
+	# Find contours in the edged image
+	contours, _ = cv2.findContours(image=closed_image, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+
+	# Find the largest contour which should be the sudoku grid
+	# ! Potential bug here if the largest contour is not the sudoku grid (it will take alot of time to fix so leaving as is for now)
+	for contour in sorted(contours, key=cv2.contourArea, reverse=True):
+		peri = cv2.arcLength(curve=contour, closed=True)
+		corners = cv2.approxPolyDP(curve=contour, epsilon=0.02 * peri, closed=True)
+
+		if len(corners) == 4:
+			return corners.reshape(4, 2).astype(np.float32)
+
+	raise ValueError("No valid grid found in the image.")
+
+def warp_image(image: Image, corners: npt.NDArray[np.float32], size: int = 450) -> Image:
+	"""
+		Warps the input image to a top-down view of the sudoku grid based on the detected corners.
+
+		Parameters:
+		- image: np.ndarray, the original input image.
+		- corners: np.ndarray, the corners of the detected grid.
+		- size: int, the desired size of the output warped image (default is 450x450).
+
+		Returns:
+		- np.ndarray, the warped image of the sudoku grid.
+	"""
+
+	# Define the destination points for the perspective transform
+	corners = _order_corners(corners)
+	dest_corners = np.array([[0, 0], [size, 0], [size, size], [0, size]], dtype=np.float32)
+
+	# Compute the perspective transform matrix and apply it
+	transform_matrix = cv2.getPerspectiveTransform(src=corners, dst=dest_corners)
+	warped_image = cv2.warpPerspective(src=image, M=transform_matrix, dsize=(size, size))
+
+	return cast(Image, warped_image)
+
+def _order_corners(corners: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+	"""
+		Orders the corners in the order: top-left, top-right, bottom-right, bottom-left.
+
+		Parameters:
+		- corners: np.ndarray, the corners to be ordered.
+
+		Returns:
+		- np.ndarray, the ordered corners.
+	"""
+
+	sorted_corners = np.zeros_like(corners)
+	sorted_corners[0] = corners[np.argmin(corners.sum(axis=1))]  # Top-left has the smallest sum
+	sorted_corners[2] = corners[np.argmax(corners.sum(axis=1))]  # Bottom-right has the largest sum
+	sorted_corners[1] = corners[np.argmin(np.diff(corners, axis=1))]  # Top-right has the smallest difference
+	sorted_corners[3] = corners[np.argmax(np.diff(corners, axis=1))]  # Bottom-left has the largest difference
+
+	return sorted_corners
