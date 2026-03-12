64b3d7416398e4263d0805eeaca2711d58694334
Philipp Knoch <knokh.ph@gmail.com>
2026-03-12 22:16:26 +0100

    updated tests

--- tests/test_scripts/testing.py
+++ tests/test_scripts/testing.py
@@ -2,7 +2,7 @@ from sudoku_omakase.core.sudoku_image import SudokuImage
 
 def test_sudoku_image():
     image_path = "tests/test_images/IMG_0120.jpg"
-    sudoku_image = SudokuImage(image_path, "BIG")
+    sudoku_image = SudokuImage(image_path, "NORMAL")
     print(sudoku_image)
     sudoku_image.solve()
     print(sudoku_image)
