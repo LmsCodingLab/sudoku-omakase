ec08a62265ad258e8589f11970e6b7c352d35cd1
Philipp <111361294+kn0kh@users.noreply.github.com>
2026-03-12 22:16:49 +0100

    89 rework pipeline logic (#90)

--- tests/test_scripts/testing.py
+++ tests/test_scripts/testing.py
@@ -1,42 +1,14 @@
-from sudoku_omakase.pipeline import SudokuGridSolver, SudokuImageSolver
-from sudoku_omakase.vision.sudoku_image import SudokuImage
-from sudoku_omakase.core.sudoku import Sudoku
-from sudoku_omakase.model.models import ModelType
-from pathlib import Path
+from sudoku_omakase.core.sudoku_image import SudokuImage
 
-def test_guess_num():
-    source = Path("tests/test_images/IMG_0120.jpg")
-    # Create a SudokuImage instance with a sample image and model type
-    sudoku_image = SudokuImage(source, model_type=ModelType.BAD)
-    
-    # Preprocess the image to get the warped version
-    warped_image = sudoku_image.preprocess()
-    
-    # Extract the fields from the warped image
-    fields = sudoku_image.extract_fields()
-    
-    # Extract the predicted numbers from the fields
-    predicted_numbers = sudoku_image.extract_numbers()
-
-    sudoku = Sudoku(predicted_numbers)
-    print(sudoku)
+def test_sudoku_image():
+    image_path = "tests/test_images/IMG_0120.jpg"
+    sudoku_image = SudokuImage(image_path, "NORMAL")
+    print(sudoku_image)
+    sudoku_image.solve()
+    print(sudoku_image)
     
 
-def test_pipelines():
-    source = "tests/test_images/IMG_0120.jpg"
-    solver = SudokuImageSolver(source, model_type="BAD")
-    solver.get_sudoku_from_image()
-    sudoku = solver.get_sudoku()
-    gird = sudoku.board.copy()
-    print(sudoku)
-    solver.solve()
-    print(sudoku)
-
-    grid_solver = SudokuGridSolver(gird)
-    print(grid_solver.sudoku)
-    grid_solver.solve()
-    print(grid_solver.sudoku)
 
 
 if "__main__" == __name__:
-    test_pipelines()
+    test_sudoku_image()
